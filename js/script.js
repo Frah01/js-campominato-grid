faf6ea322fd30284f0fe1bbd15e01b4c10b4f61f
Frah01 <vinario.francesco@gmail.com>
2022-12-06 18:50:18 +0100

    board completa

--- js/script.js
+++ js/script.js
@@ -3,16 +3,23 @@ function squares(){
     const classeElemento = document.createElement('div');
     classeElemento.classList.add('box')
     return classeElemento
+
 }
+
+
 
 let contentboxes = document.getElementById('content-box')
+let document.getElementById
 
 for(let i = 1; i<=100 ; i++){
-
-    
+    const numeri = i
     const createboxes= squares();
+    createboxes.innerText += numeri
     contentboxes.appendChild(createboxes);
     createboxes.addEventListener('click', function(){
-        this.classList.add('pulsante')
+    this.classList.add('pulsante')
+    console.log(createboxes.innerText)
+    
     })
 }
+
