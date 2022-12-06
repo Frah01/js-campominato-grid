d8fe3a68f108c0effdd4428fa9e4c4b77a8c1bfe
Frah01 <vinario.francesco@gmail.com>
2022-12-06 17:59:15 +0100

    prima funzione fatta con boxes applicati

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function squares(){
+
+    const classeElemento = document.createElement('div');
+    classeElemento.classList.add('box')
+    return classeElemento
+}
+
+let contentboxes = document.getElementById('content-box')
+
+for(let i = 1; i<=100 ; i++){
+
+    const createboxes= squares();
+
+    contentboxes.appendChild(createboxes)
+}
