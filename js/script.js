0f51f568177375106a97749cc451900476062863
Frah01 <vinario.francesco@gmail.com>
2022-12-06 18:08:22 +0100

    aggiunto il click

--- js/script.js
+++ js/script.js
@@ -9,7 +9,10 @@ let contentboxes = document.getElementById('content-box')
 
 for(let i = 1; i<=100 ; i++){
 
+    
     const createboxes= squares();
-
-    contentboxes.appendChild(createboxes)
+    contentboxes.appendChild(createboxes);
+    createboxes.addEventListener('click', function(){
+        this.classList.add('pulsante')
+    })
 }
