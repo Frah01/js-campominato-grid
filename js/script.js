eea3795bc21776a87bceda5037b4d707c89c07bf
Frah01 <vinario.francesco@gmail.com>
2022-12-07 09:58:41 +0100

    Aggiustato pulsante

--- js/script.js
+++ js/script.js
@@ -9,11 +9,8 @@ function squares(){
 
 let bottone = document.getElementById('spawn')
 let contentboxes = document.getElementById('content-box')
-
 bottone.addEventListener('click', function(){
-document.getElementById("spawn").disabled = true 
-
-
+document.getElementById('content-box').innerHTML = " "
     for(let i = 1; i<=100 ; i++){
         const numeri = i
         const createboxes= squares();
